6f5f93b1fe75987ed6c7ce936b12051f9424b1f6
Student P_08 <studentp_08@Student-P-08.local>
2017-09-13 12:42:08 +0530

    Initial Commit

---
+++ day8_iOS_tableViewCell_Asnmnt1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day8_iOS_tableViewCell_Asnmnt1
+//
+//  Created by Student P_08 on 13/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ day8_iOS_tableViewCell_Asnmnt1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day8_iOS_tableViewCell_Asnmnt1
+//
+//  Created by Student P_08 on 13/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
